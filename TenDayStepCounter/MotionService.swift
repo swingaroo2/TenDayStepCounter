087ad10dd441718385fcebaa40d4519cfba68d1f
Zach Lockett-Streiff <zachls17@gmail.com>
2017-06-27 02:51:56 -0500

    Implemented DispatchGroups

---
+++ TenDayStepCounter/MotionService.swift
@@ -0,0 +1,70 @@
+//
+//  MotionService.swift
+//  TenDayStepCounter
+//
+//  Created by Zach Lockett-Streiff on 6/26/17.
+//  Copyright © 2017 Swingaroo2. All rights reserved.
+//
+
+import Foundation
+import CoreMotion
+
+class MotionService
+{
+    let calendar  = Calendar.current
+    let pedometer = CMPedometer()
+    var dataArr: Array<CMPedometerData> = []
+    let queue = DispatchQueue(label: "StepCounterQueue", attributes: DispatchQueue.Attributes.concurrent)
+    
+    func getDailyStepDataUntilToday(numberOfPreviousDays:Int, dispatchGroup:DispatchGroup)
+    {
+        guard let date = self.getDateForPastDay(numDaysAgo: numberOfPreviousDays) else
+        {
+            NSLog("\(#function): Failed to query data with nil Date")
+            return
+        }
+        
+        self.dataArr = Array<CMPedometerData>(repeating: CMPedometerData(), count: numberOfPreviousDays)
+        var fromDate = date
+        let yesterday = self.calendar.date(byAdding: .day, value: -1, to: Date())!
+        
+        while fromDate < yesterday
+        {
+            guard let toDate = self.calendar.date(byAdding: .day, value: 1, to: fromDate) else
+            {
+                NSLog("\(#function): Failed to get previous day's date")
+                return
+            }
+            self.getStepDataForDates(fromDate, end: toDate, dispatchGroup: dispatchGroup)
+            
+            fromDate = toDate
+        }
+    }
+    
+    private func getStepDataForDates(_ start:Date, end:Date, dispatchGroup:DispatchGroup)
+    {
+        dispatchGroup.enter()
+        self.pedometer.queryPedometerData(from: start, to: end, withHandler: { [weak self] (data, error) -> Void in
+            guard let data = data else
+            {
+                if let error = error
+                {
+                    NSLog(error.localizedDescription)
+                }
+                NSLog("\(#function): Failed to get step data with error")
+                return
+            }
+            self?.dataArr.insert(data, at: 0)
+            print("\(data)")
+            dispatchGroup.leave()
+        })
+    }
+    
+    private func getDateForPastDay(numDaysAgo:Int) -> Date?
+    {
+        let date = Date()
+        let pastDate = self.calendar.date(byAdding: .day, value: -numDaysAgo, to: date)
+        print("\(pastDate!)")
+        return pastDate
+    }
+}
--- TenDayStepCounter/ViewController.swift
+++ TenDayStepCounter/ViewController.swift
@@ -7,50 +7,30 @@
 //
 
 import UIKit
-import CoreMotion
 
 class ViewController: UIViewController
 {
-
-    let pedometer = CMPedometer()
-    
-    var currentDate: Date {
-        return Date()
-    }
-    
-    var oldestDate: Date? {
-        var timeInterval = DateComponents()
-        timeInterval.day = -10
-        let dateTenDaysAgo = Calendar.current.date(byAdding: timeInterval, to: Date())
-        return dateTenDaysAgo
-    }
+    let numPreviousDays = 10
+    let motionService = MotionService()
+    let dispatchGroup = DispatchGroup()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        print()
-        
-        self.pedometer.queryPedometerData(from: self.oldestDate!, to: self.currentDate, withHandler: { (data, error) -> Void in
-            if let data = data
-            {
-                print("Data: \(data)")
-            } else if let error = error {
-                print("Error: \(error)")
-            }
-        })
-        
         // Note: use simulated data for unsupported devices
-        
         // Check if current device is supported
+        
+        self.motionService.getDailyStepDataUntilToday(numberOfPreviousDays: self.numPreviousDays, dispatchGroup: self.dispatchGroup)
+        
+        self.dispatchGroup.notify(queue: DispatchQueue.main, execute:{
+            print("===Refresh table view===")
+        })
     }
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
-        
-        // Dispose of any resources that can be recreated.
     }
-
 }
 
